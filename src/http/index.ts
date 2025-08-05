6bc7a2047577bbd7efaed8972716a0a9ae649d2e
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-05 14:32:41 +0300

    update

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,20 @@
+// src/http/index.ts
 import axios from "axios";
 const baseURL = import.meta.env.VITE_BASE_URL;
 
-const http = axios.create({
-  baseURL,
+const http = axios.create({ baseURL });
+
+// İstek interceptor'ı: her istekten önce token'ı header'a ekler
+http.interceptors.request.use((config) => {
+  const session = JSON.parse(localStorage.getItem("sb-kwzxuscknzzjnafalzdo-auth-token") || "{}"); // veya sadece token
+  const token = session?.access_token;
+  console.log(localStorage)
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default http;
